19cc46635913333238c7ede9ffb3738772866fb5
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-05-07 09:53:04 +0200

    Convert tabs on espaces

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,7 +2,7 @@
 from add_0 import add
 
 if __name__ == "__main__":
-	a = 1
-	b = 2
-	result = add(a, b)
-	print("{a} + {b} = {result}".format(a=a, b=b, result=result))
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{a} + {b} = {result}".format(a=a, b=b, result=result))
