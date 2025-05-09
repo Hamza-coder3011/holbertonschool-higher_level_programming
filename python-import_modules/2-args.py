3b952601197b472da88dad80373e9c65fd25578a
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-05-09 15:18:14 +0200

    Resolve Tasks from 2 to 5 without the task 4

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+
+def main():
+    arg_count = len(argv) - 1
+    if arg_count == 0:
+        print("0 arguments.")
+    elif arg_count == 1:
+        print("1 argument:")
+    else:
+        print(f"{arg_count} arguments:")
+
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
+
+if __name__ == "__main__":
+    main()
