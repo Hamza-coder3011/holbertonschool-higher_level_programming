65f95c9225cc9ed8b95655542a5607c12eaeb9e5
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-05-14 11:29:08 +0200

    Correct syntaxe for the task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,8 +7,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             result = 0
-        except (TypeError, ValueError):
-            print("Wrong type")
+        except TypeError:
+            print("wrong type")
             result = 0
         except IndexError:
             print("out of range")
