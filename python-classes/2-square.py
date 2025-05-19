c2f050abffb67140cc70e975414d14b26c856913
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-05-19 17:11:43 +0200

    Correct syntaxe

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,7 +12,7 @@ class Square:
             size: The size of the square (optional, default is 0)
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an integre")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be >= ")
+            raise ValueError("size must be >= 0")
         self.__size = size
