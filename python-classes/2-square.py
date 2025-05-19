bb560663ef08417c04e85c8ed55420a455798c50
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-05-19 17:02:15 +0200

    Task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class Square with validated size attribute"""
+
+
+class Square:
+    """Class that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+        
+        Args:
+            size: The size of the square (optional, default is 0)
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integre")
+        if size < 0:
+            raise ValueError("size must be >= ")
+        self.__size = size
