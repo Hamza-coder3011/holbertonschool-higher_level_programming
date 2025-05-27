aaa3d207a13ba421af996ac11bb225c3328f0c26
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-05-27 10:58:53 +0200

    Modified syntaxe

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -16,9 +16,9 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-        """Compute and return the area of the rectangle"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
     def __str__(self):
         """Returns a string representation of the rectangle"""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
