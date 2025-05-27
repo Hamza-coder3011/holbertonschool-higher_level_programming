d6c5efea7b22c309d91f0eaad3dd4c5b784c073d
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-05-27 12:47:24 +0200

    Correct pycodestyle

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -21,10 +21,10 @@ class Circle(Shape):
     """Circle shape class"""
     def __init__(self, radius):
         self.radius = radius
-    
+
     def area(self):
         return math.pi * self.radius ** 2
-    
+
     def perimeter(self):
         return 2 * math.pi * self.radius
 
@@ -35,10 +35,10 @@ class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
         self.height = height
-    
+
     def area(self):
         return self.width * self.height
-    
+
     def perimeter(self):
         return 2 * (self.width + self.height)
 
