9e72386730242439c1c7b83578a76670aa80cb94
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-06-26 11:47:40 +0200

    Update syntaxe task 5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-'''Lists all cities of a given state'''
+"""Lists all cities of a given state from the database hbtn_0e_4_usa."""
 
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     user = sys.argv[1]
@@ -10,13 +11,21 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=user, passwd=pwd, db=db_name)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=pwd,
+        db=db_name
+    )
 
     cursor = db.cursor()
-    query = ("SELECT cities.name FROM cities "
-             "JOIN states ON cities.state_id = states.id "
-             "WHERE states.name = %s ORDER BY cities.id ASC;")
+    query = (
+        "SELECT cities.name FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC"
+    )
     cursor.execute(query, (state_name,))
     rows = cursor.fetchall()
 
