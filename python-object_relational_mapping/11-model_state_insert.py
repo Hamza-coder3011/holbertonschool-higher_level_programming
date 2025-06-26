e9a9af45b060dc3d0e0508a91b8f99a740ffd221
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-06-26 11:52:53 +0200

    Resolve task 11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -7,22 +7,19 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-def main():
-    """Creates and adds 'Louisiana' state, then prints its id."""
-    username = sys.argv[1]
-    password = sys.argv[2]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
     db_name = sys.argv[3]
 
-    # Connexion SQLAlchemy
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        f"mysql+mysqldb://{user}:{pwd}@localhost:3306/{db_name}",
         pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Création de l'objet State
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
@@ -30,7 +27,3 @@ def main():
     print(new_state.id)
 
     session.close()
-
-
-if __name__ == "__main__":
-    main()
