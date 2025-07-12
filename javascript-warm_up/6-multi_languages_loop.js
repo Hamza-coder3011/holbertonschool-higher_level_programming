acd5e7a5e532a23962158026e8fe28cd5a6c0113
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-07-12 22:32:55 +0200

    task 6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const lines = ['C is fun', 'Python is coll', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < lines.length; i++) console.log(lines[i]);
