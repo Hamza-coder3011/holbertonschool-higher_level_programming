3e9d4039b8b30c9b5a4cc0f6493f2513c490a8d2
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-07-12 22:25:15 +0200

    task 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 const argc = process.argv.length - 2;
 
 if (argc === 0) {
-	console.log('NO argument');
+	console.log('No argument');
 } else if (argc === 1) {
 	console.log('Argument found');
 } else {
