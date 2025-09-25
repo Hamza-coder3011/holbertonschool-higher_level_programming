4f507e977c406d3b80ae716d7cf3746433140995
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-09-25 13:43:08 +0200

    task 1

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -34,10 +34,8 @@ class Rectangle(Shape):
         self.width = width
         self.height = height
 
-
     def area(self):
         return self.width * self.height
-
 
     def perimeter(self):
         return 2 * (self.width + self.height)
