061cc3dad93ec0994118a3abb474e302ac58a452
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-09-25 11:14:17 +0200

    Task 0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        """Abstract method that must be implemented by subclasses"""
+        pass
+
+
+class Dog(Animal):
+
+    def sound(self):
+        return "Bark"
+
+
+class Cat(Animal):
+
+    def sound(self):
+        return "Meow"
