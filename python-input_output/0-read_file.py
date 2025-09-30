310c4e53ccab429593896d9bded62aa6917b307d
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-09-30 11:05:22 +0200

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,7 @@ and print its content to stdout.
 """
 
 
-def  read_file(filename=""):
+def read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
