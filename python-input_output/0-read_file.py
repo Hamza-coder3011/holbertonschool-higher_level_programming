df170553d792a8b1480ce25156d95528ad60c947
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-09-30 11:04:15 +0200

    Update syntax

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read a UTF-8 text file
+and print its content to stdout.
+"""
+
+
 def  read_file(filename=""):
     """Reads a UTF-8 text file and prints its content to stdout"""
     with open(filename, encoding="utf-8") as f:
