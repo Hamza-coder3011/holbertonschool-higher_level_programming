69d39f4f0f6335f5dd2f1cf3cb26393c12c0e871
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-09-30 15:20:03 +0200

    Task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns
+    the number of characters written.
+
+    Args:
+    filename (str): Name of the file to write to.
+    text (str): The string content to write.
+
+    Returns:
+    int: Number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
