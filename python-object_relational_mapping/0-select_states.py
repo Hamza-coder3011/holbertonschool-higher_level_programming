cd220832b849d49447b471572cdeeb7b5a022fc7
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-10-23 12:24:04 +0200

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python3
+"""
+This script lists all states from a given MySQL database.
+
+Usage:
+    ./0-select_states.py <mysql_username> <mysql_password> <database_name>
+
+Arguments:
+    mysql_username    MySQL username
+    mysql_password    MySQL password
+    database_name     Name of the database containing the 'states' table
+
+Functionality:
+    - Connects to a MySQL server on localhost at port 3306
+    - Retrieves all rows from the 'states' table
+    - Sorts the results by 'id' in ascending order
+    - Prints each row as a tuple (id, name)
+
+Requirements:
+    - Python3
+    - MySQLdb module (install via 'pip3 install mysqlclient')
+"""
 
 import MySQLdb
 import sys
