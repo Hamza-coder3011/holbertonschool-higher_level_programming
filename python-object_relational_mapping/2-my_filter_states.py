729cfb48d8dc0f28a847de23a0db55a8655d0f40
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-10-24 14:38:38 +0200

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,4 +1,4 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """
 Displays all values in the 'states' table of 'hbtn_0e_0_usa'
 where 'name' matches the argument provided.
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     database = sys.argv[3]
     state_name = sys.argv[4]
 
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -44,16 +45,18 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     query = (
-        "SELECT * FROM states "
+        "SELECT * FROM states"
         "WHERE name LIKE BINARY '{}'"
         "ORDER BY id ASC"
     ).format(state_name)
 
     cursor.execute(query)
 
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
+
 
     cursor.close()
     db.close()
