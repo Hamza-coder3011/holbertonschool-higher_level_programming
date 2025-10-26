3d22bae26f2eda4e51239e330b8f9486524f7aa8
Hamza-coder3011 <hammadi.hamza@hotmail.com>
2025-10-26 15:48:35 +0100

    correct syntax task 10

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -8,6 +8,7 @@ import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
+from sqlalchemy import func
 
 
 if __name__ == "__main__":
